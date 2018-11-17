61b2aa651e819f80d25186891397dbb3b3c74c5c
Arthur <athong@csumb.edu>
2018-11-17 10:05:31 +0000

    Updating dbconnection info

--- lab5/dbconnection.php
+++ lab5/dbconnection.php
@@ -5,11 +5,11 @@
         $password='';
         
         if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-            $url = parse_url(getenv("mysql://bdcbf0758f5ed3:a0b1ef4d@us-cdbr-iron-east-01.cleardb.net/heroku_3d87761e8bb2af6?reconnect=true"));
-            $host = $url["host"];
-            $dbname = substr($url["path"], 1);
-            $username = $url["user"];
-            $password = $url["pass"];
+            //$url = parse_url(getenv("mysql://bdcbf0758f5ed3:a0b1ef4d@us-cdbr-iron-east-01.cleardb.net/heroku_3d87761e8bb2af6?reconnect=true"));
+            $host = "us-cdbr-iron-east-01.cleardb.net";
+            $dbname = "heroku_3d87761e8bb2af6";
+            $username = "bdcbf0758f5ed3";
+            $password = "a0b1ef4d";
         }
 
         $dbConn= new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
